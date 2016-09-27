5727f946e164b0e9d935112f81373b4e587dd6ba
Array Huang <hyw125@qq.com>
2016-09-27 17:57:22 +0800

    eslint开cache

--- webpack-config/vendor/eslint.config.js
+++ webpack-config/vendor/eslint.config.js
@@ -2,5 +2,7 @@ var path = require('path');
 var dirVars = require('../base/dir-vars.config.js');
 module.exports = {
   configFile: path.resolve(dirVars.staticRootDir, './.eslintrc'),
+  failOnWarning: true,
   failOnError: true,
+  cache: true,
 };
