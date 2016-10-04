1e73b21788b2596e7be3af610709d13d0802461e
Array Huang <hyw125@qq.com>
2016-10-04 16:11:16 +0800

    给DllPlugin和DllReferencePlugin添点儿注释

--- webpack-config/inherit/plugins.config.js
+++ webpack-config/inherit/plugins.config.js
@@ -23,9 +23,9 @@ var commonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin({
 var extractTextPlugin = new ExtractTextPlugin('[name]/styles.css');
 
 var dllPlugin = new webpack.DllReferencePlugin({
-  context: dirVars.staticRootDir,
-  manifest: require('../../manifest.json'),
-  name: 'dll',
+  context: dirVars.staticRootDir, // 指定一个路径作为上下文环境，需要与DllPlugin的context参数保持一致，建议统一设置为项目根目录
+  manifest: require('../../manifest.json'), // 指定manifest.json
+  name: 'dll',  // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
 });
 
 var configPlugins = [providePlugin, commonsChunkPlugin, extractTextPlugin, dllPlugin];
--- webpack-dll.config.js
+++ webpack-dll.config.js
@@ -1,14 +1,18 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const dirVars = require('./webpack-config/base/dir-vars.config.js');
+const dirVars = require('./webpack-config/base/dir-vars.config.js'); // 与业务代码共用同一份路径的配置表
 
 module.exports = {
   output: {
     path: dirVars.dllDir,
     filename: '[name].js',
-    library: '[name]',
+    library: '[name]', // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
   },
   entry: {
+    /*
+      指定需要打包的js模块
+      或是css/less/图片/字体文件等资源，但注意要在module参数配置好相应的loader
+    */
     dll: [
       'jquery', '!!bootstrap-webpack!bootstrapConfig',
       'metisMenu/metisMenu.min', 'metisMenu/metisMenu.min.css',
@@ -16,18 +20,19 @@ module.exports = {
   },
   plugins: [
     new webpack.DllPlugin({
-      path: 'manifest.json',
-      name: '[name]',
-      context: dirVars.staticRootDir,
+      path: 'manifest.json', // 本Dll文件中各模块的索引，供DllReferencePlugin读取使用
+      name: '[name]',  // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与参数output.library保持一致
+      context: dirVars.staticRootDir, // 指定一个路径作为上下文环境，需要与DllReferencePlugin的context参数保持一致，建议统一设置为项目根目录
     }),
+    /* 跟业务代码一样，该兼容的还是得兼容 */
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
       'window.$': 'jquery',
     }),
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin('[name].css'), // 打包css/less的时候会用到ExtractTextPlugin
   ],
-  module: require('./webpack-config/module.config.js'),
-  resolve: require('./webpack-config/resolve.config.js'),
+  module: require('./webpack-config/module.config.js'), // 沿用业务代码的module配置
+  resolve: require('./webpack-config/resolve.config.js'), // 沿用业务代码的resolve配置
 };
