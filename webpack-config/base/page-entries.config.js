c7e9535dfee12f765361671c1bd2a7f127bf1245
Array Huang <hyw125@qq.com>
2016-10-14 11:14:20 +0800

    自动按协定好的文件目录结构找寻webpack的入口，取代过去手动指定的做法

--- webpack-config/base/page-entries.config.js
+++ webpack-config/base/page-entries.config.js
@@ -1,6 +1,8 @@
-module.exports = [
-  'index/login',
-  'index/index',
-  'alert/index',
-  'user/edit-password', 'user/modify-info',
-];
+var glob = require('glob');
+var dirVars = require('./dir-vars.config.js');
+var options = {
+  cwd: dirVars.pagesDir, // 在pages目录里找
+  sync: true, // 这里不能异步，只能同步
+};
+var globInstance = new glob.Glob('*/*', options);
+module.exports = globInstance.found; // 一个数组，形如['index/index', 'index/login', 'alert/index']
