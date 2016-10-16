8b3a1e7b4db602fbccbb3ef41b54372d112bf4c0
Array-Huang <hyw125@gmail.com>
2016-10-16 15:53:19 +0800

    稍微整理一下plugin的配置

--- webpack-config/inherit/plugins.config.js
+++ webpack-config/inherit/plugins.config.js
@@ -5,30 +5,29 @@ var path = require('path');
 var dirVars = require('../base/dir-vars.config.js');
 var pageArr = require('../base/page-entries.config.js');
 
-/* 全局shimming */
-var providePlugin = new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  'window.jQuery': 'jquery',
-  'window.$': 'jquery',
-});
-
-/* 抽取出所有通用的部分 */
-var commonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin({
-  name: 'commons',      // 需要注意的是，chunk的name不能相同！！！
-  filename: '[name].bundle.js',
-  minChunks: 4,
-});
-
-var extractTextPlugin = new ExtractTextPlugin('[name]/styles.css');
-
-var dllPlugin = new webpack.DllReferencePlugin({
-  context: dirVars.staticRootDir, // 指定一个路径作为上下文环境，需要与DllPlugin的context参数保持一致，建议统一设置为项目根目录
-  manifest: require('../../manifest.json'), // 指定manifest.json
-  name: 'dll',  // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
-});
-
-var configPlugins = [providePlugin, commonsChunkPlugin, extractTextPlugin, dllPlugin];
+var configPlugins = [
+  /* 全局shimming */
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    'window.$': 'jquery',
+  }),
+  /* 抽取出所有通用的部分 */
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'commons',      // 需要注意的是，chunk的name不能相同！！！
+    filename: '[name].bundle.js',
+    minChunks: 4,
+  }),
+  /* 抽取出chunk的css */
+  new ExtractTextPlugin('[name]/styles.css'),
+  /* 配置好Dll */
+  new webpack.DllReferencePlugin({
+    context: dirVars.staticRootDir, // 指定一个路径作为上下文环境，需要与DllPlugin的context参数保持一致，建议统一设置为项目根目录
+    manifest: require('../../manifest.json'), // 指定manifest.json
+    name: 'dll',  // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
+  }),
+];
 
 pageArr.forEach((page) => {
   const htmlPlugin = new HtmlWebpackPlugin({
