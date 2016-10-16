5960b8c332efb898d9bb1592ede08d990405e567
Array-Huang <hyw125@gmail.com>
2016-10-16 20:34:59 +0800

    在自动查找页面入口时，忽略以开头的目录

--- webpack-config/base/page-entries.config.js
+++ webpack-config/base/page-entries.config.js
@@ -4,5 +4,5 @@ var options = {
   cwd: dirVars.pagesDir, // 在pages目录里找
   sync: true, // 这里不能异步，只能同步
 };
-var globInstance = new glob.Glob('*/*', options);
+var globInstance = new glob.Glob('!(_)*/!(_)*', options); // 考虑到多个页面共用HTML等资源的情况，跳过以'_'开头的目录
 module.exports = globInstance.found; // 一个数组，形如['index/index', 'index/login', 'alert/index']
