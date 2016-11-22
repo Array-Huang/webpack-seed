11593e7965dda70a9328b877b1b3a1cf5e86d62a
Array-Huang <hyw125@gmail.com>
2016-11-22 10:55:59 +0800

    用了压缩插件会导致所有loader添加min配置，而autoprefixser也被定格到某个browers配置

--- webpack-config/plugins.product.config.js
+++ webpack-config/plugins.product.config.js
@@ -1,11 +1,12 @@
 var webpack = require('webpack');
 var pluginsConfig = require('./inherit/plugins.config.js');
 
-pluginsConfig.push(new webpack.optimize.UglifyJsPlugin({
-  compress: {
-    warnings: false,
-  },
-}));
+/* webpack1下，用了压缩插件会导致所有loader添加min配置，而autoprefixser也被定格到某个browers配置 */
+// pluginsConfig.push(new webpack.optimize.UglifyJsPlugin({
+//   compress: {
+//     warnings: false,
+//   },
+// }));
 
 pluginsConfig.push(new webpack.DefinePlugin({
   IS_PRODUCTION: true,
