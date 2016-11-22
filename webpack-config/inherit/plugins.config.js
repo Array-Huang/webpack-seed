e26227a94c91f38ac74f49fae23588a42cc45101
Array-Huang <hyw125@gmail.com>
2016-11-22 16:16:29 +0800

    引入webpack-bundle-analyzer生成打包文件结构的可视化分析报告；执行npm run start的时候自动打开浏览器

--- webpack-config/inherit/plugins.config.js
+++ webpack-config/inherit/plugins.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var path = require('path');
 var dirVars = require('../base/dir-vars.config.js');
 var pageArr = require('../base/page-entries.config.js');
@@ -26,6 +27,12 @@ var configPlugins = [
     context: dirVars.staticRootDir, // 指定一个路径作为上下文环境，需要与DllPlugin的context参数保持一致，建议统一设置为项目根目录
     manifest: require('../../manifest.json'), // 指定manifest.json
     name: 'dll',  // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
+  }),
+  /* 生成打包文件结构信息的json文件，供'webpack-bundle-analyzer'作为可视化分析报告的数据源 */
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'disabled', // 不立即生成可视化分析报告
+    generateStatsFile: true, // 确定生成打包文件结构信息的json文件
+    statsFilename: 'stats.json', //json文件的文件名，完整路径是参考webpack的配置项output.path
   }),
 ];
 
