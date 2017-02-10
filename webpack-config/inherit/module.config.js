a875e2ace731ed540376cc13d4ae20796023b6f0
Array-Huang <hyw125@gmail.com>
2017-02-10 15:25:17 +0800

    为升级webpack2做准备

--- webpack-config/inherit/module.config.js
+++ webpack-config/inherit/module.config.js
@@ -2,7 +2,7 @@ var dirVars = require('../base/dir-vars.config.js');
 module.exports = {
   preLoaders: [{
     test: /\.js$/,
-    loader: 'eslint',
+    loader: 'eslint-loader',
     include: dirVars.srcRootDir,
     exclude: [/bootstrap/],
   }],
@@ -10,41 +10,46 @@ module.exports = {
   loaders: [
     {
       test: require.resolve('jquery'),
-      loader: 'expose?$!expose?jQuery',
+      loader: 'expose-loader?$!expose-loader?jQuery',
     },
     {
       test: /\.js$/,
       include: dirVars.srcRootDir,
-      loader: 'babel-loader?presets[]=es2015-loose&cacheDirectory&plugins[]=transform-runtime',
+      loader: 'babel-loader',
+      query: {
+        presets: [['es2015', { loose: true }]],
+        cacheDirectory: true,
+        plugins: ['transform-runtime'],
+      },
     },
     {
       test: /\.html$/,
       include: dirVars.srcRootDir,
-      loader: 'html',
+      loader: 'html-loader',
     },
     {
       test: /\.ejs$/,
       include: dirVars.srcRootDir,
-      loader: 'ejs',
+      loader: 'ejs-loader',
     },
     {
       // 图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
       // 如下配置，将小于8192byte的图片转成base64码
       test: /\.(png|jpg|gif)$/,
       include: dirVars.srcRootDir,
-      loader: 'url?limit=8192&name=./static/img/[hash].[ext]',
+      loader: 'url-loader?limit=8192&name=./static/img/[hash].[ext]',
     },
     {
       // 专供iconfont方案使用的，后面会带一串时间戳，需要特别匹配到
       test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
       include: dirVars.srcRootDir,
-      loader: 'file?name=static/fonts/[name].[ext]',
+      loader: 'file-loader?name=static/fonts/[name].[ext]',
     },
     {
       // 专供bootstrap方案使用的，忽略bootstrap自带的字体文件
       test: /\.(woff|woff2|svg|eot|ttf)$/,
       include: /bootstrap/,
-      loader: 'null',
+      loader: 'null-loader',
     },
   ],
 };
